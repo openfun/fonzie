fc02ca3bc6a0195192cd825ce0823af4821df448
Julien Maupetit <julien@maupetit.net>
2018-02-20 12:15:35 +0100

    fix(compat) fix urls namespacing
    Add app_name for included urls with namespace

--- fonzie/urls/__init__.py
+++ fonzie/urls/__init__.py
@@ -6,9 +6,10 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import include, url
 
+from ..apps import FonzieConfig
 from . import status as status_urls
 
-
+app_name = FonzieConfig.name
 urlpatterns = [
     url(r'status/', include(status_urls, namespace='status')),
 ]
--- fonzie/urls/status.py
+++ fonzie/urls/status.py
@@ -6,8 +6,10 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
 
+from ..apps import FonzieConfig
 from ..views.status import VersionView
 
+app_name = FonzieConfig.name
 urlpatterns = [
     url(r'^version$', VersionView.as_view(), name='version'),
 ]
