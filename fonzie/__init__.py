cba277a5b7899bca895bb3ecdb11bb4936331b55
Julien Maupetit <julien@maupetit.net>
2019-04-19 17:31:59 +0200

    🚨(isort) fix modules import
    A recent update of isort required to fix an incorrect import statements
    ordering.

--- fonzie/__init__.py
+++ fonzie/__init__.py
@@ -4,9 +4,9 @@ Fonzie, A FUN API for Open edX.
 from __future__ import absolute_import, unicode_literals
 
 from os import path
-from setuptools.config import read_configuration
 
 import pkg_resources
+from setuptools.config import read_configuration
 
 
 def _extract_version(package_name):
@@ -19,11 +19,11 @@ def _extract_version(package_name):
         return pkg_resources.get_distribution(package_name).version
     except pkg_resources.DistributionNotFound:
         _conf = read_configuration(
-            path.join(path.dirname(path.dirname(__file__)), 'setup.cfg')
+            path.join(path.dirname(path.dirname(__file__)), "setup.cfg")
         )
-    return _conf['metadata']['version']
+    return _conf["metadata"]["version"]
 
 
-__version__ = _extract_version('fonzie')
+__version__ = _extract_version("fonzie")
 
-default_app_config = 'fonzie.apps.FonzieConfig'  # pylint: disable=invalid-name
+default_app_config = "fonzie.apps.FonzieConfig"  # pylint: disable=invalid-name
