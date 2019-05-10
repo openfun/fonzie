7051a36e5a852e2fd9ce1289a2b18c43fafba3e3
Richard Moch <richard.moch@gmail.com>
2019-05-10 11:37:52 +0200

    🐳(project) improve working environment for real life usage
    Many modifications in order to use Fonzie to fix openedx-docker
    issue #106: (CSV export files access control)
    
    - Add cms container and import script to allow demo-course import
    - Add Nginx container to serve media files
    - Add unit tests django settings and related modifications to pytest commands
    - Restructure volumes mounting to ease development
    - Use docker-compose volumes instead of tempfs for macosx compatibility

--- edx-platform/config/lms/docker_run_development.py
+++ edx-platform/config/lms/docker_run_development.py
@@ -1,20 +1,19 @@
-from docker_run_production import *
+from .docker_run_production import *
 
 
 DEBUG = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-SECRET_KEY = "foo"
-
 PIPELINE_ENABLED = False
-STATICFILES_STORAGE = 'openedx.core.storage.DevelopmentStorage'
 
-STATIC_ROOT = '/data/static/lms'
-STATIC_URL = '/static/'
-MEDIA_ROOT = '/data/media'
+STATICFILES_STORAGE = "openedx.core.storage.DevelopmentStorage"
+
+STATIC_ROOT = "/edx/app/edxapp/data/static"
+STATIC_URL = "/static/"
+MEDIA_ROOT = "/edx/app/edxapp/data/media"
 LOG_DIR = '/data/log'
 
-FEATURES['ENABLE_DISCUSSION_SERVICE'] = False
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+SECRET_KEY = "foo"
 
 ALLOWED_HOSTS = ["*"]
 
@@ -27,11 +26,33 @@ INSTALLED_APPS += (
     'fonzie',
 )
 
-ROOT_URLCONF = 'lms.api_urls'
+ROOT_URLCONF = "lms.api_urls"
+
+# Disable CourseTalk service (student course reviewing)
+COURSE_REVIEWS_TOOL_PROVIDER_FRAGMENT_NAME = None
+COURSE_REVIEWS_TOOL_PROVIDER_PLATFORM_KEY = None
 
 # Django Rest Framework (aka DRF)
 REST_FRAMEWORK = {
     'ALLOWED_VERSIONS': ('1.0', ),
     'DEFAULT_VERSION': '1.0',
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+}
+
+FEATURES['ENABLE_DISCUSSION_SERVICE'] = False
+
+FEATURES["AUTOMATIC_AUTH_FOR_TESTING"] = True
+FEATURES["RESTRICT_AUTOMATIC_AUTH"] = False
+
+FEATURES["ENABLE_GRADE_DOWNLOADS"] = True
+FEATURES["ALLOW_COURSE_STAFF_GRADE_DOWNLOADS"] = True
+FEATURES["ENABLE_ASYNC_ANSWER_DISTRIBUTION"] = False
+FEATURES["ENABLE_INSTRUCTOR_BACKGROUND_TASKS"] = False
+
+GRADES_DOWNLOAD = {
+   "STORAGE_CLASS": "django.core.files.storage.FileSystemStorage",
+   "STORAGE_KWARGS": {
+       "location": "/data/export",
+       "base_url": "/api/v1.0/protected/instructor_files",
+   }
 }
---
+++ edx-platform/config/lms/docker_run_test.py
@@ -0,0 +1,33 @@
+from docker_run_development import *
+
+
+ENVIRONMENT = 'test'
+
+TEST_ROOT = "/data/test_root"
+COMMON_TEST_DATA_ROOT = COMMON_ROOT / "test" / "data"
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': TEST_ROOT + '/db/fun.db'
+        },
+    }
+
+os.environ["EDXAPP_TEST_MONGO_HOST"] = "mongodb"
+os.environ["EDXAPP_TEST_MONGO_PORT"] = "27017"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SECRET_KEY = "foo"
+
+PIPELINE_ENABLED = False
+
+STATICFILES_STORAGE = 'openedx.core.storage.DevelopmentStorage'
+
+FEATURES['ENABLE_DISCUSSION_SERVICE'] = False
+
+ALLOWED_HOSTS = ["*"]
+
+LOGGING['handlers'].update(
+    local={'class': 'logging.NullHandler'},
+    tracking={'class': 'logging.NullHandler'},
+)
